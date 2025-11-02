459dcac515440d0d6d522736a148a4a8848bab52
DominicDev <s0l0estudi4r@gmail.com>
2025-11-02 14:08:14 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = ' KINA';
+export let nameSongs = [
+	"GET YOU THE MOON", /*1*/
+	"CAN WE KISS FOREVER? ", /*2*/
+	"TELL ME ABOUT YOU", /*3*/
+	"FEEL AGAIN", /*4*/
+	"WISH I WAS BETTER", /*5*/
+	"I'M NOT GOING BACK", /*6*/
+	"PARACHUTE", /*7*/
+	"I'LL ALWAYS WAIT 4 U", /*8*/
+	"THINGS I WANTED TO TELL YOU", /*9*/
+	"U NOT THERE", /*10*/
+	"I STILL DREAM OF YOU", /*11*/
+	"ARE YOU STILL WITH HIM", /*12*/
+	"I'VE CHANGED FOR YOU", /*13*/
+	"SAY U DON'T LOVE ME ANYMORE", /*14*/
+	"NEVERMIND", /*15*/
+	"TRYING NOT TO CRY", /*16*/
+	"U’RE MINE", /*17*/
+	"BABY YOU'RE WORTH IT", /*18*/
+	"I NEED U", /*19*/
+	"KNIFE IN MY HEART", /*20*/
+]
